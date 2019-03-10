0a934c7cf0c06ed363f13a5ed52337548052f0fa
Daniel Melody <dmwalter12@gmail.com>
2019-03-10 17:28:34 -0400

    fixed 4s and 9s

--- js-algos-data-structs/roman-numeral-converter/roman-numeral-converter.js
+++ js-algos-data-structs/roman-numeral-converter/roman-numeral-converter.js
@@ -3,25 +3,49 @@
 function convertToRoman(num) {
   var roman = "";
 
-  // iterate while NUM > 0
-  while (num > 0) {
-    // check 3 and under
-    if (num <= 3) {
-         roman = roman + "I";
-         num = num - 1;
-    }  else if (num <= 4) {
-         roman = roman + "IV";
-         num = num - 4;
-    }  else if (num <= 5) {
-         roman = roman + "V";
-         num = num - 5;
-    }  else if (num <= 10) {
-         roman = roman + "X";
-         num = num - 10;
-    }
-  }
-
-    
+	// iterate while NUM > 0
+	while (num > 0) {
+		if (num >= 1000) {
+			roman = roman + "M";
+			num = num - 1000;
+		} else if (num >= 900) {
+			roman = roman + "CM";
+			num = num - 900;
+		} else if (num >= 500) {
+			roman = roman + "D";
+			num = num - 500;
+		} else if (num >= 400) {
+			roman = roman + "CD";
+			num = num - 400;
+		} else if (num >= 100) {
+      roman = roman + "C";
+			num = num - 100;
+		} else if (num >= 90) {
+			roman = roman + "XC";
+			num = num - 90;
+		} else if (num >= 50) {
+			roman = roman + "L";
+			num = num - 50;
+		} else if (num >= 40) {
+			roman = roman + "XL";
+			num = num - 40;
+		} else if (num >= 10) {
+			roman = roman + "X";
+			num = num - 10;
+		} else if (num >= 9) {
+			roman = roman + "IX";
+			num = num - 9;
+		} else if (num >= 5) {
+			roman = roman + "V";
+			num = num - 5;
+		} else if (num >= 4) {
+			roman = roman + "IV";
+			num = num - 4;
+		} else {
+			roman = roman + "I";
+			num = num - 1;
+		}
+  }    
   // return ROMAN string value
   return roman;
 }
