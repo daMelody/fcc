bc917c48da4617fefaa63ccedfc5cf14ec11c679
Daniel Melody <dmwalter12@gmail.com>
2019-03-10 16:13:36 -0400

    adding palindrome.js, almost working properly

---
+++ js-algos-data-structs/palindrome/palindrome.js
@@ -0,0 +1,29 @@
+// Palindrome Checker challenge from FCC
+
+function palindrome(str) {
+  // use REGEX to keep alphanumeric characters
+  var regex = /[a-z0-9]/ig;
+
+  // build NEWSTRING of proper chars
+  var newString = "";
+  for (var i=0; i<str.length; i++) {
+    if (regex.test(str[i])) {
+      newString = newString + str[i];  
+    }
+  }
+
+  // convert NEWSTRING to lowercase
+  newString = newString.toLowerCase();
+
+  // build REVERSE of NEWSTRING
+  var reverse = newString.split("").reverse().join("");
+
+  // compare NEWSTRING and REVERSE
+  if (newString === reverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+palindrome("eye");
