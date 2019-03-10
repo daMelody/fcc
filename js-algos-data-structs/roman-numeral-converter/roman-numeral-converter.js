5440e75d3e03157626268c3c24124f13768da628
Daniel Melody <dmwalter12@gmail.com>
2019-03-10 16:51:58 -0400

    constructed if-else chain to handle cases

--- js-algos-data-structs/roman-numeral-converter/roman-numeral-converter.js
+++ js-algos-data-structs/roman-numeral-converter/roman-numeral-converter.js
@@ -5,5 +5,31 @@ function convertToRoman(num) {
 
   // iterate while NUM > 0
   while (num > 0) {
-  // use SWITCH up to   
+
+  // if-else chain
+  if (num > 1000) {
+    roman = roman + "M";
+    num = num - 1000;
+  } else if (num > 500) {
+    roman = roman + "D";
+    num = num - 500;
+  } else if (num > 100) {
+    roman = roman + "C";
+    num = num - 100;
+  } else if (num > 50) {
+    roman = roman + "L";
+    num = num - 50;
+  } else if (num > 10) {
+    roman = roman + "X";
+    num = num - 10;
+  } else if (num > 5) {
+    roman = roman + "V";
+    num = num - 5;
+  } else {
+    roman = roman + "I";
+    num = num - 1;
+  }
+
+  // return ROMAN string value
+  return roman;
 }
