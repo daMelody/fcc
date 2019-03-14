9dafd7272a9130bd9d5144f24394618c29541ca6
Daniel Melody <dmwalter12@gmail.com>
2019-03-14 12:02:59 -0400

    getting registerTotal

--- js-algos-data-structs/cash-register/cash-register.js
+++ js-algos-data-structs/cash-register/cash-register.js
@@ -5,5 +5,12 @@ function checkCashRegister(price, cash, cid) {
   var change = cash - price;
   var values = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100];
 
-  
+  // get registerTotal
+  var registerTotal = 0;
+  for (let i=0; i<cid.length; i++) {
+    registerTotal += cid[i][1];
+  }
+
+  // round registerTotal to nearest cent
+  registerTotal = Math.round(100*registerTotal)/100;
 }
