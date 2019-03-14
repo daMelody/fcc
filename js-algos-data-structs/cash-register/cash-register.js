8ea7800c7d2ae1039cc2f6f5f4443170efcf0a29
Daniel Melody <dmwalter12@gmail.com>
2019-03-14 12:00:04 -0400

    making Register object and values array

---
+++ js-algos-data-structs/cash-register/cash-register.js
@@ -0,0 +1,9 @@
+// Cash Register Challenge from FreeCodeCamp
+
+function checkCashRegister(price, cash, cid) {
+  let register = new Register();
+  var change = cash - price;
+  var values = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100];
+
+  
+}
